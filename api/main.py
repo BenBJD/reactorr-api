0670fd7d18744ac734da3028a82fef228630be32
Ben Davison <ben@bjdavison.co.uk>
2021-04-17 23:21:10 +0100

    Fixed CORS temporarily with wildcard

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import requests
 import os.path
@@ -21,6 +22,17 @@ config = {
     "jackett_apikey": database.get_config("jackett_apikey")
 }
 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/search")
 async def get_users(indexer: str, category: int, q: str):
